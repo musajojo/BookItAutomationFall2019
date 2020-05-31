aca24444cad0a3b65607226c9bb4d200b506b013
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-31 17:44:10 -0400

    commit

--- src/test/java/com/bookit/database/UserDB.java
+++ src/test/java/com/bookit/database/UserDB.java
@@ -1,4 +1,17 @@
 package com.bookit.database;
 
+import com.bookit.utilities.DBUtility;
+
 public class UserDB {
+
+    /**
+     * If count equals to 1 - users exist
+     * @param email of user to look up
+     * @return true, if user exist
+     */
+    public boolean checkIfUserExistInDB(String email) {
+        String query = "SELECT COUNT(*) AS count FROM users WHERE email = '" + email + "'";
+        int countOfUsers = (int) DBUtility.getCellValue("count");
+        return countOfUsers == 1;
+    }
 }
