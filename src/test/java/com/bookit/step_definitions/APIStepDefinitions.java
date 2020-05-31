386ed82daaee8da1ecc7637c930483f882df56f6
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-05-31 12:23:30 -0400

    commit

--- src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
@@ -13,6 +13,7 @@ import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -71,5 +72,17 @@ public class APIStepDefinitions {
         List<String> roomNames = response.jsonPath().getList("name");
         Assert.assertTrue(roomNames.containsAll(dataTable));
         MatcherAssert.assertThat(roomNames, hasItem(in(dataTable)));
+    }
+    //   When user sends POST request to "/api/students/student" with following information:
+    @When("user sends POST request to {string} with following information:")
+    public void user_sends_POST_request_to_with_following_information(String path, List<Map<String, String>> dataTable) {
+        for(Map<String, ?> user: dataTable) {
+            response = given().queryParams(user).contentType(contentType).auth().oauth2(token).when().post(path).prettyPeek();
+        }
+    }
+
+    @Then("user deletes previously added students")
+    public void user_deletes_previously_added_students(List<Map<String, String>> dataTable) {
+
     }
 }
